8831a505930e7855a951cc77bb609b7ffe2e046f
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-26 18:00:45 +0100

    Empty files and main function.

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/26 17:58:00 by msousa            #+#    #+#             */
+/*   Updated: 2022/03/26 18:00:24 by msousa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __ZOMBIE_H__
+#define __ZOMBIE_H__
+
+// ************************************************************************** //
+//                               Zombie Class                                 //
+// ************************************************************************** //
+
+class Zombie {
+
+
+public:
+
+	typedef Zombie		t;
+
+	// static int	getNbAccounts( void );
+
+	Zombie( void );
+	~Zombie( void );
+
+	// void	displayStatus( void ) const;
+
+
+private:
+
+	// static int	_nbAccounts;
+
+	// static void	_displayTimestamp( void );
+
+	// int				_accountIndex;
+
+};
+
+#endif /* __ZOMBIE_H__ */
