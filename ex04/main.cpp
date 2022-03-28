a2f993679f80eb21471f50938927e9724a127b64
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-28 20:52:41 +0200

    Complete ex04.

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,14 +6,69 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 17:56:49 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/27 22:18:01 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/28 20:50:41 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-int	main( void )
+void    replace(std::string& line, const std::string& s1, const std::string& s2)
 {
+    size_t	start = 0;
 
+	while (true)
+	{
+		size_t	i = line.find(s1, start);
+
+		if (i == std::string::npos)
+			break ;
+
+		line = line.substr(0, i) + s2 + line.substr(i + s1.length());
+		start = i + s2.length();
+	}
+}
+
+int	main( int argc, char* argv[] )
+{
+	if (argc != 4) {
+		ERROR("arguments <filename> <s1> <s2> required");
+		return 1;
+	}
+
+	std::ifstream   in(argv[1], std::ifstream::in);
+
+	if (in.fail()) {
+		ERROR("opening file");
+        return 2;
+    }
+
+	std::string     filename(argv[1]);
+    std::ofstream   out((filename + ".replace").c_str());
+
+    if (out.fail()) {
+		ERROR("creating file");
+        return 3;
+    }
+
+    std::string     s1(argv[2]);
+    std::string     s2(argv[3]);
+
+	if (!s1.length()) {
+        ERROR("string to replace can't be empty");
+        return 4;
+    }
+
+	while (!in.eof())
+	{
+        std::string 	line;
+        std::getline(in, line);
+
+		replace(line, s1, s2);
+
+		out << line << std::endl;
+    }
+
+    in.close();
+    out.close();
 	return 0;
 }
--- ex04/main.hpp
+++ ex04/main.hpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 17:58:00 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/27 22:18:41 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/28 19:58:01 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 #define __MAIN_H__
 
 # include <iostream>
+# include <fstream>
 
 # define LOG(x) std::cout << x << std::endl
+# define ERROR(x) std::cerr << "Error: " << x << std::endl
 
 #endif /* __MAIN_H__ */
